30edf9abcf443c2358fbef6eee43ceb73afca4cf
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-04-23 00:16:33 +0200

    Navbar development

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { Turn as Hamburger } from "hamburger-react";
+import { useState } from "react";
 
 function Navbar() {
+  //state which defines whether burger icon is clicked or not
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  //function which changes the state of the burger icon
+  const handleMenuClick = () => {
+    setMenuOpen(!isMenuOpen);
+  };
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+    <nav className="navbar">
+      <img src="" alt="logo du portfolio" />
+      <p>Mon Portfolio</p>
+      {/* hamburger component with onclick handleMenuClick */}
+      <Hamburger toggled={isMenuOpen} toggle={handleMenuClick} />
+      <div
+        className={`navbar-links ${isMenuOpen ? "menu-open" : "menu-hidden"}`}
+      >
         <Link className="navbar-brand" to="/">
           Home
         </Link>
